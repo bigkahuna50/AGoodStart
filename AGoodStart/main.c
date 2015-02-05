db3b16a8e6afbaa689920cd8387912954c265566
Sascha Messner <saschamessner@Saschas-MacBook-Pro.local>
2015-02-04 19:52:22 -0800

    Initial Commit

---
+++ AGoodStart/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Sascha Messner on 2/4/15.
+//  Copyright (c) 2015 Sascha Messner. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
